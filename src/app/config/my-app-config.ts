0dcf594fd3f90eb3c652e5f97b38df3137701185
Ramesha Fernando <rameshafernando1@gmail.com>
2023-08-06 15:29:03 +0530

    first commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaapd9z8dYlA55VJ5d7',
+        issuer: 'https://dev-02077211.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
